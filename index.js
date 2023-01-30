d44231d539def070752748a2ae4335b7fc8380d1
Felix Cho <felix.cho.kr@gmail.com>
2023-01-30 12:06:12 +0900

    rollback to original version for backward compatibility

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let jsonpathmap = require("./lib/jsonpathmap");
-let jsonpathmap2 = require("./lib/jsonpathmap2");
+// let jsonpathmap2 = require("./lib/jsonpathmap2");
 
 module.exports = jsonpathmap;
-module.exports = jsonpathmap2;
+// module.exports = jsonpathmap2;
